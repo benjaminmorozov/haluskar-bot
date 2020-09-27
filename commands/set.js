05feac44be4330fdf707eb8607a800bbf7315550
Benjamín Morozov <benjaminmorozov@gmail.com>
2020-09-27 16:20:56 +0200

    boop

--- commands/set.js
+++ commands/set.js
@@ -1,7 +1,4 @@
 exports.run = (client, message, args) => {
-                                     //owner                designer             main admin           admin                main mod             mod                  helper
-    if(!message.member.roles.some(r=>["610704273822711820","622715668659437568","631922921475932170","616501517058310184","645728270519631889","610704593558437899","614694328119459840"].includes(r.id)) )
-    return message.reply("you don't have enough permissions to execute this command!");
     // makes the bot say something and delete the message. As an example, it's open to anyone to use.
     // To get the "message" itself we join the `args` back into a string with spaces:
     if (!args.length) {
