9eb83e9025dd599b74785cf7dd0a0a71b114392d
Benjamín Morozov <benjaminmorozov@gmail.com>
2020-10-01 10:06:17 +0200

    boop

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -13,7 +13,7 @@ exports.run = (client, message, args) => {
       .setAuthor(member.username)
       .setThumbnail(member.displayAvatarURL)
       .addField('**Full Username:**', member.username + `#` + member.discriminator, false)
-      .addField('**Member ID:**', member.id, false)
+      .addField('**Member ID:**', '**' + member.id + '**', false)
       .addField('**Account Creation Date:**', `${creationDate()}`, false)
       .addField('**Member Join Date:**', `${moment.utc(message.guild.member(member).joinedAt).format('dddd DD/MM/YYYY')}`, false)
       .setFooter('Thanks for being a part of our community. ❤️', message.guild.iconURL);
