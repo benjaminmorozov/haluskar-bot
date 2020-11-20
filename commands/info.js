34685a9faa73b61cd412d26a54ba67d87bb924d0
Benjamín Morozov <33388822+benjaminmorozov@users.noreply.github.com>
2020-11-20 13:51:40 +0100

    a
    a

--- commands/info.js
+++
@@ -1,3 +0,0 @@
-exports.run = (client, message, args) => {
-    message.channel.send(`Thanks for asking! 🥰 My name's Haluskar, I was originally designed by Andromatic to moderate multiple czechoslovak hacking servers. This project began on the 28th of october, 2019 and since, I\'ve changed a lot. You can use the command **${client.config.prefix}help** to display the list of all my commands. Otherwise, if you have any technical questions about me, or you want to report a bug, use the command **${client.config.prefix}report**. Have a nice day! 😉`);
-};
--- commands/userinfo.js
+++ commands/userinfo.js
@@ -16,6 +16,6 @@ exports.run = (client, message, args) => {
       .addField('**Member ID:**', '**' + member.id + '**', false)
       .addField('**Account Creation Date:**', `${creationDate()}`, false)
       .addField('**Member Join Date:**', `${moment.utc(message.guild.member(member).joinedAt).format('dddd DD/MM/YYYY')}`, false)
-      .setFooter('Thanks for being a part of our community. ❤️', message.guild.iconURL);
+      .setFooter('Halušky 2.0 ❤️', message.guild.iconURL);
     message.channel.send(userinfoEmbed);
 };
