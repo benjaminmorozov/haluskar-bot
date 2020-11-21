866fd0c0f23e5d1c04b5c07839b9e2fdb4032ce7
Benjamín Morozov <33388822+benjaminmorozov@users.noreply.github.com>
2020-11-21 16:42:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-    client.login('NzU5NzcwMDczMDU2OTM1OTQ2.X3CVBQ.7vrBxYCc9s8zgPM7CkeUQESeO8k');
-    bot.user.setStatus('available')
-    bot.user.setPresence({
-        game: {
-            name: 'with depression',
-            type: "STREAMING",
-            url: "https://www.twitch.tv/monstercat"
-        }
-    });
+    client.login('NzU5NzcwMDczMDU2OTM1OTQ2.X3CVBQ.KrvU0MgB0SiZLkkSJzEN9PzBkcA');
     console.log("running");
 })
